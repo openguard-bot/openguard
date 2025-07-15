7b693e8544ddce43ef5747f39e2b11d1df9611ea
Slipstream <me@slipstreamm.dev>
2025-07-15 02:27:20 -0600

    fix(dashboard): Remove unused autoprefixer import

--- dashboard/frontend/vite.config.js
+++ dashboard/frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-import autoprefixer from 'autoprefixer';
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dashboard/',
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [react(), tailwindcss()],
     resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
